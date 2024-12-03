69185923bdddad31beb2a7798798ded65698646c
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-03 02:03:49 -0500

    useState hook, used useState to set initial state and update state of entire app

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_04/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_04/start/react-app/src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 
-const [firstCity, second] = [
-  "Tokyo",
-  "Tahoe City",
-  "Bend"
-];
+function App() {
+  // we want to get a better understanding of what useState is and what is expects
+  const what = useState();
+  console.log(what);
+  // Output: (2) [undefined, ƒ]
+  // First value: think of the undefined value as the state value when we run the app for the first time  (when we render the app component)
+  // Second value: function we will use to update the state, changes state for the entire app
+  // So -> how do we set an initial state for this app?
 
-console.log(firstCity);
-console.log(second);
+  // States can only update/respond to some kind of user input
+  const [emotion, setEmotion] = useState("happy"); // Emotion is our state variable, setEmotion is the function we are going to use to update the state
 
-function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
