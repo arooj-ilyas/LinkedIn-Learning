73541ad80b95c967fd2275622ebaf971e05a6df7
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-03 01:35:39 -0500

    INSTALLED dependecies and worked with hot reloading

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_02/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_02/start/react-app/src/App.js
@@ -1,26 +1,13 @@
 // Typically components are kept inside of their own files
 // General format -> create a function, export function as default (one function per component)
+// Now we can make changes which hot reload, refresh browser
 
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from React</h1>
     </div>
   );
 }
