b26e70fe02854d903e28a78fe2eab49ee1d97362
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-03 01:48:51 -0500

    LEARNT array destructuring

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
@@ -1,13 +1,27 @@
 /* Notes:
 - Important to understand destructuring, helps us get ready to deal with state in our application
+- We can use destructuring to grab properties from the props object by their key
+- In this example, think of the object as having a key called library, a value called React
+- We can select the Library key and now we don't have to access it via props.library, we can grab directly from the object (i.e. reaching into the object and grabbing its key)
 */
 
 import "./App.css";
 
-function App(props) {
+// You can also destructure arrays
+// Unlike when destructuring objects, we can't reach into the array and assign a property name based on a key
+// const cities = ["Tokyo", "Tahoe City", "Bend"];
+
+// We need to do array destructuring
+// In this, we assign a variable name based on the position within the array
+const [firstCity, secondCity, thirdCity] = ["Tokyo", "Tahoe City", "Bend"];
+console.log(firstCity);
+console.log(secondCity);
+console.log(thirdCity);
+
+function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from {props.library}</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
