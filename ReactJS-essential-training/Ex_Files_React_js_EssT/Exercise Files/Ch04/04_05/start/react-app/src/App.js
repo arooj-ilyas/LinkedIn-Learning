d855bf6261b63eebc571ca31edecf1cd84784f43
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-03 02:14:22 -0500

    ADDED useEffect iwth a console.log message

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_05/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_05/start/react-app/src/App.js
@@ -1,20 +1,31 @@
+/* NOTES:
+UseEffect
+- Another hook built into React, typically used to manage side effects that aren't related to a components render (i.e. console messages, loading data, animations)
+- UseEffects is sideEffect because -> not something which are affected by the render, but more so something that happens alongside it
+- Rather than making it equal to some array like useState, we pass in am empty function to this
+*/
+
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
 
+  /*
+    useEffects take in two arguments...
+    1: empty function -> define what effect we want to happen
+    2: dependency array -> when the effect is actually being called
+    ---> if you pass in an empty array [], it means it will only happen on mount (e.g. when app is first rendered once and not going to be called again)
+  */
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, []);
+
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>
-        Sad
-      </button>
-      <button
-        onClick={() => setEmotion("excited")}
-      >
-        Excited
-      </button>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
