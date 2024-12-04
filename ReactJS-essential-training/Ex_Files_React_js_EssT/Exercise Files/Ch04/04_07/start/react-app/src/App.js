f772d2f9e6aa9e306a3520741924d6022d6dbeef
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-04 01:32:45 -0500

    CREATED a checked variable set to false initially to manage the state of the checkbox

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
@@ -6,6 +6,8 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [checked, setChecked] = useState(false);
+
   return (
     <div className="App">
       <input type="checkbox"></input>
