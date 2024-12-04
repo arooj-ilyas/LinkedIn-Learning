64c5004b9a200cbe5e2c9fef250e4e210a7008da
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-04 01:30:14 -0500

    REMOVED useEffect logic to begin creating a checkbox that is going to manage states with useState

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
@@ -1,38 +1,12 @@
+/* NOTES:
+- Here we are going to create a checkbox that is going to manage states with useState
+*/
+
 import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] =
-    useState("tired");
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
-  return (
-    <div className="App">
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>
-        Sad
-      </button>
-      <button
-        onClick={() => setEmotion("excited")}
-      >
-        Excited
-      </button>
-      <h2>
-        Current secondary emotion is {secondary}.
-      </h2>
-      <button
-        onClick={() => setSecondary("grateful")}
-      >
-        Grateful
-      </button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
