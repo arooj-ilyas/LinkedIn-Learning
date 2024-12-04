8ab45e3ae0a8d28806445b4f7398246e908d82e0
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-04 01:22:24 -0500

    CREATED a plan for what to do i this file

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_06/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_06/start/react-app/src/App.js
@@ -1,8 +1,12 @@
+// We can use useEffect and useState to keep track of different variables
+// A dependency array can rely on more than one thing, i.e. carry out the useEffect depending on more than one condition
+
 import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
 
   useEffect(() => {
     console.log(`It's ${emotion} around here!`);
@@ -11,14 +15,8 @@ function App() {
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>
-        Sad
-      </button>
-      <button
-        onClick={() => setEmotion("excited")}
-      >
-        Excited
-      </button>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
