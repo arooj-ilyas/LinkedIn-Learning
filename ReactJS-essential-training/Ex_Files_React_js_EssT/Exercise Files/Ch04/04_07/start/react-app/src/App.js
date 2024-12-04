fb20a7cb5a169ab7520572535e469b12a9c3d85b
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-04 01:44:01 -0500

    UPDATED plan in notes of how we are going to swap out useState for useReducer to be tidier

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
@@ -6,6 +6,15 @@
   - We want to have out label reflect that change by checking if checked is true and displaying the corresponding msg (use ? and :)
 */
 
+/* REFACTORING to utilize useReducer:
+- We can extract this functionality -> setChecked((checked) => !checked) -> to be 'cleaner' into a separate function so we don't manage it within our onChange
+- Swap out useState for a different hook, useReducer
+- useReducer takes in two different arguments
+  - First: function we are going to use to update the state
+  - Second: our initial state
+- Then we update the onChange to take in the name of the function we will use to update the state
+*/
+
 import "./App.css";
 import { useState } from "react";
 
