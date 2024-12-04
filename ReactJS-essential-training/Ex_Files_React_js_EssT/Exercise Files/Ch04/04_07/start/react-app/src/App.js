b3d5d07e848e67f8bc5c505ec4b80ad43b6d5cff
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-04 01:45:06 -0500

    REFACTORED useSTate for useReducer because onChange was becoming complex

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/start/react-app/src/App.js
@@ -16,20 +16,14 @@
 */
 
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
 
   return (
     <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => {
-          setChecked((checked) => !checked);
-        }}
-      ></input>
+      <input type="checkbox" value={checked} onChange={setChecked}></input>
       <label>{checked ? "Checked" : "Not Checked"}</label>
     </div>
   );
