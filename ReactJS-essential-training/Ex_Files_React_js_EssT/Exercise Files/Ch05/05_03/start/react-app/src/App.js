ccf1f53071d81440ad790820355df9645e4fb96f
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-10 15:11:37 -0500

    ADDED logic for useInput custom hook to perform when called

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch05/05_03/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch05/05_03/start/react-app/src/App.js
@@ -1,7 +1,30 @@
+// WRAP UP
+/*
+- We've seen how react hooks can add additional functionality to our applications
+- useState hook -> to manage state of form fields
+- useRef hook -> to reach out to inputs and figure out their vaues ref 
+- useEffect -> to perform side effects
+
+-> MOST exciting thing about hooks = can create own custom hooks to be used within app! 
+- A custom hook is a function that always starts with the keyword 'use'
+- NOTE: want to use spread operator {...} sparingly, only when not pushing in too many elements
+*/
+
 import "./App.css";
 import { useState } from "react";
 
 function App() {
+  function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue); // any input is going to take in a value and use setValue to upadate that value
+    return [
+      {
+        value,
+        onChange: (e) => setValue(e.target.value),
+      }, // first item in the array is the state value, as well as an onChange which takes in whatever the input is and calls setValue with the target.value
+      () => setValue(initialValue), // second item we are going to pass is the cleanup function where we setValue to whatever the initialValue was
+    ];
+  }
+
   const [title, setTitle] = useState("");
   const [color, setColor] = useState("#000000");
   const submit = (e) => {
@@ -14,18 +37,14 @@ function App() {
     <form onSubmit={submit}>
       <input
         value={title}
-        onChange={(event) =>
-          setTitle(event.target.value)
-        }
+        onChange={(event) => setTitle(event.target.value)}
         type="text"
         placeholder="color title..."
       />
       <input
         value={color}
         type="color"
-        onChange={(event) =>
-          setColor(event.target.value)
-        }
+        onChange={(event) => setColor(event.target.value)}
       />
       <button>ADD</button>
     </form>
