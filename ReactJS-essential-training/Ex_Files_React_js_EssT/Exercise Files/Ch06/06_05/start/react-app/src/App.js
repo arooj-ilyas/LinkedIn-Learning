0c6ab7730345834b65dbd66781d7c49957db40f0
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-10 16:24:03 -0500

    CREATED a data set with name and elevation gains in feet

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_05/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_05/start/react-app/src/App.js
@@ -4,6 +4,13 @@
 
 import "./App.css";
 
+const tahoe_peaks = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 },
+];
+
 function App() {
   return <h1>Hello</h1>;
 }
