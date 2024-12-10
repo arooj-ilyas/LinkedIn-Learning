2cee8b8b3755cf776328cc834e36a3e73c2bf9d9
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-10 15:24:29 -0500

    ADDED: notes to explain learnings of this lesson

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch05/05_03/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch05/05_03/start/react-app/src/App.js
@@ -7,7 +7,14 @@
 
 -> MOST exciting thing about hooks = can create own custom hooks to be used within app! 
 - A custom hook is a function that always starts with the keyword 'use'
+- A hook is just a function we can use for any sort of repeatable code; should be very reusable and can be shared across the project, write tests for this function
+- In this specific case, we can use this inside of the form any time we need to process some user input 
+  ==> now you can create mutliple form fields that take something in, we handle a change event and we can handle resetting the form when submitted
 - NOTE: want to use spread operator {...} sparingly, only when not pushing in too many properties
+- NOTE: In some cases, we can streamline dev by using form libraries...
+   -> https://formik.org/ allows you to handle submitting formstaking in values; take in initial values and think about form holistically as a collection of different fields
+   -> https://react-hook-form.com/ helps with creating forms that use validation; uses react hooks and had other advanced features to create scalable forms
+   -> https://usehooks.com/ not form specific but ton of resources/hooks; think of these as recipes for creating types of hooks with lots of common use cases 
 */
 
 import "./App.css";
