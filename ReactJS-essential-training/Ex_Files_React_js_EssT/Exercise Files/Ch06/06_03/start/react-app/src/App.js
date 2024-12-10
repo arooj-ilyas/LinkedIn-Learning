e450b3be55936af07c7ade4f494d0290be97d26b
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-10 15:59:23 -0500

    CREATED states for loading and error

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_03/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_03/start/react-app/src/App.js
@@ -1,3 +1,16 @@
+/*
+- When fetching data from external API, our data can be in a few different states which can all be represented using useState hooks
+
+1 = Loading state
+- fetching data but hasn't yet come back
+
+2 = Success state
+- we have some data to display
+
+3 = Error state
+- what we want to display if something goes wrong
+*/
+
 import "./App.css";
 import { useState, useEffect } from "react";
 
@@ -13,6 +26,9 @@ function GithubUser({ name, location, avatar }) {
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     fetch(`https://api.github.com/users/arooj-ilyas`)
       .then((response) => response.json())
