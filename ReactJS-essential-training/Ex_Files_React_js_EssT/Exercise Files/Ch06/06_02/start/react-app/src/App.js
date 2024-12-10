e9c45fb506252d723b3b48f918ab705582d3ba17
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-10 15:51:39 -0500

    ADDED avatar property to component

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_02/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_02/start/react-app/src/App.js
@@ -6,11 +6,12 @@ In this lesson:
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GithubUser({ name, location }) {
+function GithubUser({ name, location, avatar }) {
   return (
     <div>
       <h1>{name}</h1>
       <h1>{location}</h1>
+      <img src={avatar} height={150} alt={name} />
     </div>
   );
 }
@@ -22,7 +23,14 @@ function App() {
       .then((response) => response.json())
       .then(setData);
   }, []);
-  if (data) return <GithubUser name={data.name} location={data.location} />;
+  if (data)
+    return (
+      <GithubUser
+        name={data.name}
+        location={data.location}
+        avatar={data.avatar_url}
+      />
+    );
   return <h1>Data</h1>;
 }
 
