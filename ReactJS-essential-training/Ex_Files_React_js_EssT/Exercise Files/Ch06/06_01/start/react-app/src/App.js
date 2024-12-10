5fa616e1cb61ecffc1b4c9ace892e577cb8a6b15
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-10 15:36:17 -0500

    UPDATED useEffect logic

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_01/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch06/06_01/start/react-app/src/App.js
@@ -9,7 +9,14 @@ import { useState, useEffect } from "react"; // useState to handle data, useEffe
 function App() {
   // Create container to store data
   const [data, setData] = useState(null);
-  useEffect(() => {});
+  useEffect(() => {
+    // fetch is built into the browser; its a way of making a HTTPS call to get some data from some sort of source
+    fetch(`https://api.github.com/users/arooj-ilyas`)
+      // once we get that data back, we are going to take that response and call .json to turn response into jason
+      .then((response) => response.json())
+      // then we are going to chain on another dot function which will setData to the response in the correct format
+      .then(setData);
+  }, []);
 
   return <h1>Data</h1>;
 }
