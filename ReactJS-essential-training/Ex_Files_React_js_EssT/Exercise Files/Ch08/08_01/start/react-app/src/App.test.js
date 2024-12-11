e3d1bacbc585032778805434e4512aada61ceef1
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 15:32:43 -0500

    ADDED notes for how to run test

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_01/start/react-app/src/App.test.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_01/start/react-app/src/App.test.js
@@ -1,4 +1,6 @@
 // You can use command npm test to run all of the tests within the app
+// npm test will run anything with .test.js extension
+// In this case, test is failing because it expects screen to say 'learn react'
 
 import { render, screen } from "@testing-library/react";
 import App from "./App";
