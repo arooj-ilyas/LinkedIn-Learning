54635fe82e40ab164693a2e3cb3f89bdb23e4e24
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 15:42:03 -0500

    COMPLETED test function with expected output for given value

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_02/start/react-app/src/functions.test.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_02/start/react-app/src/functions.test.js
@@ -9,5 +9,5 @@
 import timesTwo from "./functions";
 
 test("Multiples by two", () => {
-  expect(timesTwo(4)).toBe(8);
+  expect(timesTwo(4)).toBe(8); // write out a matcher with the expected output -> https://jestjs.io/docs/using-matchers
 });
