6bdc6c44f534131f89155a00cd75e13f6e19fc71
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 14:44:07 -0500

    ADDED: react router npm package

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_01/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_01/start/react-app/src/App.js
@@ -1,10 +1,17 @@
+/*
+- On every website, important to have some kind of mechanism to be able to move from page to page
+- In react, we're dealing with components, so we want to figure out how to move from component to component when dealing with a single page app
+  - Solution: https://reactrouter.com/
+  - Allows you to seamlessly move from page to page; displaying different components 
+*/
+
 import "./App.css";
 
 const tahoe_peaks = [
   { name: "Freel", elevation: 10891 },
   { name: "Monument", elevation: 10067 },
   { name: "Pyramid", elevation: 9983 },
-  { name: "Tallac", elevation: 9735 }
+  { name: "Tallac", elevation: 9735 },
 ];
 
 function List({ data, renderItem, renderEmpty }) {
@@ -13,9 +20,7 @@ function List({ data, renderItem, renderEmpty }) {
   ) : (
     <ul>
       {data.map((item) => (
-        <li key={item.name}>
-          {renderItem(item)}
-        </li>
+        <li key={item.name}>{renderItem(item)}</li>
       ))}
     </ul>
   );
