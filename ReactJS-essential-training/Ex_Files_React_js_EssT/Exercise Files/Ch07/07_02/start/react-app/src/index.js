f673c527bb35ff62be7542f3c02a7e4198624c87
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 15:02:05 -0500

    IMPORT contact component from App.js

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_02/start/react-app/src/index.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_02/start/react-app/src/index.js
@@ -8,6 +8,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 import { About } from "./App";
+import { Contact } from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
@@ -16,6 +17,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
