15a93ef63f71b4e6716aec76bd315e3ce54a3657
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 15:08:36 -0500

    ADDED link to /about from home page

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_03/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_03/start/react-app/src/App.js
@@ -1,5 +1,7 @@
 /*
 - We want to be able to link to different components without manually typing endpoint into browser
+- Reason we are importing from react-router-dom is because we are using the DOM/brownser to create this web application
+  - There are also react-router tools for React Native (mobile apps) -> each split into its on package
 */
 
 import "./App.css";
@@ -8,6 +10,9 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
       <h1>My Website</h1>
     </div>
   );
