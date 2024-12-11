8eac41176049add7b870ac25252df9a5eca100c5
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 15:22:11 -0500

    FILLED OUT nested route within about route:

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_04/start/react-app/src/index.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_04/start/react-app/src/index.js
@@ -1,12 +1,13 @@
 /*
 - Nested route configurations are really powerful because it allows you to create page heirarchy's in your app
-- Just note -> at some level we want to have our route configurations created, define all the paths and then we create the element propety to display the correct component at the correct time 
+- Just note -> at some level we want to have our route configurations created, define all the paths and then we create the element propety to display the correct component at the correct time
+- When we do nested routes, the path can omit the / (e.g. history vs /history) 
 */
 
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App, About, Contact } from "./App";
+import { App, About, Contact, History } from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
@@ -14,7 +15,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/about" element={<About />}>
-        <Route></Route>
+        <Route path="history" element={<History />} />
       </Route>
       <Route path="/contact" element={<Contact />} />
     </Routes>
