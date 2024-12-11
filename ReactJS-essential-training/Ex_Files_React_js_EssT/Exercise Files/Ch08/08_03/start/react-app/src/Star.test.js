67b7e178362503e432ab3316e7d40a807718145b
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 15:58:29 -0500

    CREATED test using react testing library to getByText element and match it to whatw e expect

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_03/start/react-app/src/Star.test.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_03/start/react-app/src/Star.test.js
@@ -4,6 +4,15 @@
     - utilising { render } from "testing-library/react"
 */
 
-import { render } from "testing-library/react";
+import { Star } from "./Star";
+import { render } from "@testing-library/react";
 
-test("renders a h1", () => {});
+// inside of callback fn, we want to destructure a react testing library query from the render of star
+// a query is a way of selecting a particular element based on some sort of property
+test("renders a h1", () => {
+  const { getByText } = render(<Star />);
+  // select the text content
+  const h1 = getByText(/Cool Star/);
+  // expect it to have the text content you write here
+  expect(h1).toHaveTextContent("Cool Star");
+});
