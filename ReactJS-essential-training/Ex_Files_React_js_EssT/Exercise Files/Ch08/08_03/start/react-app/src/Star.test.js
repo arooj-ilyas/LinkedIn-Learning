1cfcaec06a4fc52291f89a9805b3160b2afc01b5
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 15:51:57 -0500

    IMPORT render to render a file which isn't being rendered on the dom (star)

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_03/start/react-app/src/Star.test.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_03/start/react-app/src/Star.test.js
@@ -1,5 +1,9 @@
 /**
 - when creating a test, it takes in two arguments: name and callback fn
+- tests can be used to render components, even if they are not actually being rendered anywhere on the DOM
+    - utilising { render } from "testing-library/react"
 */
+
+import { render } from "testing-library/react";
 
 test("renders a h1", () => {});
