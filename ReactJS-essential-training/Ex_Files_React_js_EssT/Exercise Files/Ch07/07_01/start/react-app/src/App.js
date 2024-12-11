8f7a6dc57f34cdb08fd073166c927816e536a09d
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 14:47:33 -0500

    COMPONENT: home page

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_01/start/react-app/src/App.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_01/start/react-app/src/App.js
@@ -3,12 +3,24 @@
 - In react, we're dealing with components, so we want to figure out how to move from component to component when dealing with a single page app
   - Solution: https://reactrouter.com/
   - Allows you to seamlessly move from page to page; displaying different components 
+
+- In this exercise: create the skeleton components to be routed between
 */
 
 import "./App.css";
 
+function Home() {
+  return (
+    <div>
+      <h1>My Website</h1>
+    </div>
+  );
+}
+
 function App() {
-  return <h1>App</h1>;
+  return <Home />;
 }
 
 export default App;
+
+// Next exercise: display components through react.router
