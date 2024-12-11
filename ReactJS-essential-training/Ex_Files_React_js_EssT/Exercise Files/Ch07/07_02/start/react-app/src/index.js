2a2a3cb17d094772cf3f966a8744e178c227485d
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 14:59:01 -0500

    IMPORT About from App.js

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_02/start/react-app/src/index.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_02/start/react-app/src/index.js
@@ -7,6 +7,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import About from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
