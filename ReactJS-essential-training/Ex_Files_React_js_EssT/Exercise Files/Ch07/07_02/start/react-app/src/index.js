c943cf3ce99dcdc74126857e803fc70ee8e6b0d4
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 14:54:46 -0500

    IMPORT: routes and router from react-router-dom

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_02/start/react-app/src/index.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch07/07_02/start/react-app/src/index.js
@@ -1,10 +1,13 @@
-// First step when configuring our router is to do it at the index level of the app
+/*
+- First step when configuring our router is to do it at the index level of the app
+- We want to import BrowserRouter and wrap it around the app -> allows app and child components information about routing and where you are currently
+*/
 
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
