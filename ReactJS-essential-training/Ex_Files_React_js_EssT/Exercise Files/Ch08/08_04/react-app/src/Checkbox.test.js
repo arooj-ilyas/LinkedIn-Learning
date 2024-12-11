07495302a7da2bab95e77ff3018bcbd9f3779eff
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 16:14:05 -0500

    ADD test with automated features using fireEvent so testing file can 'click' checkbox

--- ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_04/react-app/src/Checkbox.test.js
+++ ReactJS-essential-training/Ex_Files_React_js_EssT/Exercise Files/Ch08/08_04/react-app/src/Checkbox.test.js
@@ -3,10 +3,17 @@
 - Utilize destructured queries - https://testing-library.com/docs/queries/about/
 */
 
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Checkbox } from "./Checkbox";
 
 test("Selecting checked box should change value to true", () => {
   // callback fn should contain test
   const { getByLabelText } = render(<Checkbox />);
+
+  // getByLabelText is going to look for 'not checked', adding i gets rid of case sensitivity in regex
+  const checkbox = getByLabelText(/not checked/i);
+
+  // we want to automate the process of firing an event on this checkbox, e.g. so test can interact with dom and 'click' checkbox
+  fireEvent.click(checkbox);
+  expect(checkbox.checked).toEqual(true);
 });
