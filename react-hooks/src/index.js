86afd1f16127bbdbdc23ed2c98bd7edf4361ea9d
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-12 00:03:11 -0500

    SETTING UP index.js file to fetch data from an api, and using useState and useEffect to manipulate and display this

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -7,7 +7,7 @@
 
 - useEffect side effects are typically console.logs or some kind of DOM api (e.g. history or window)
 - typically to use useEffect, you need to set up a state variable
-- useEffect takes in a function
+- useEffect takes in a function and by default runs after each render and each update, using [] makes it run only on first render
 */
 
 // for the sake of keeping things simple for this app, we won't import the app, we will just create it here; want to add state to our app component
@@ -17,22 +17,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function App() {
-  const [name, setName] = useState("Jan");
-
-  useEffect(() => {});
-
-  return (
-    <section>
-      <p>Congratulations {name}!</p>
-      <button
-        onClick={() => {
-          setName("Will");
-        }}
-      >
-        Change Winner
-      </button>
-    </section>
-  );
+  const [data, setData] = useState([]);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
