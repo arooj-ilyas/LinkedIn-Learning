a442c4683440929f8d8c084256e28d1ec02f6784
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-12 16:37:42 -0500

    CLEANUP codebase for index.js

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -12,31 +12,25 @@
 
 // for the sake of keeping things simple for this app, we won't import the app, we will just create it here; want to add state to our app component
 
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { useInput } from "./useInput";
 
 function App() {
-  // now instead of using useState, we can use our custom
-  // const [sound, setSound] = useState("");
-  // const [color, setColor] = useState("#000000");
+  // now instead of using useState, we can use our custom hook
   const [titleProps, resetTitle] = useInput("");
   const [colorProps, resetColor] = useInput("#000000");
 
-  // handle submitting the form -> want to prevent page from reloading as soon as form is submitting ('ADD' is pressed)
   const submit = (e) => {
     e.preventDefault();
     alert(`${titleProps.value} sounds like ${colorProps.value}`);
-    // once alert is made and user has hit ok, we want fields to clear
     resetColor();
     resetTitle();
   };
 
   return (
     <form onSubmit={submit}>
-      {/* instead of ref={name}, we will attach to form element using value={name*/}
-      {/* now our inputs are in charge of changing the state, not the useRef */}
       <input {...titleProps} type="text" placeholder="Sound..." />
       <input {...colorProps} type="color" />
       <button>ADD</button>
