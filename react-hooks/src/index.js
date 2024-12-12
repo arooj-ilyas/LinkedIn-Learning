b264f96158700a2639f8854255ff60fe8df1154d
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 23:27:12 -0500

    UPDATED index.js to display stars for a rating system, with helper functions and seperate components

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -2,27 +2,36 @@
 - useState expects two items in its array
   1. name of state variable we want to keep track of
   2. function to change the state of the variable 
+
+- use useState hook to keep track of value of checkbox
+
+- more often than not, we are not working with a single component, so working with state in a component tree is an important concept to grasp
 */
+
+// for the sake of keeping things simple for this app, we won't import the app, we will just create it here; want to add state to our app component
 
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-// for the sake of keeping things simple for this app, we won't import the app, we will just create it here; want to add state to our app component
+import { FaStar } from "react-icons/fa";
+
+// in order to create an array of stars which are to be displayed based on rating, we need to create a helper function
+// this function will take in the length (i.e. how many stars we want to create) and return an array with that number of items
+const createArray = (length) => [...Array(length)];
+
+// this component is what makes up all of the star rating components
+function Star() {
+  return <FaStar />;
+}
+
+// this component is going to return a certain number of stars based on the rating, and use that createArray function to display the stars
+function StarRating() {
+  return createArray(5).map((n, i) => <Star key={i} />);
+}
+
 function App() {
-  const [checked, setChecked] = useState(false);
-  return (
-    <div>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => {
-          setChecked((checked) => !checked);
-        }}
-      />
-      <label>{checked ? "checked" : "not checked"}</label>
-    </div>
-  );
+  return <StarRating />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
