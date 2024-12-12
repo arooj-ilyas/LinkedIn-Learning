ebef61ca417ca6c52e4a4ea0f7ade2c67be5e345
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 23:10:19 -0500

    STATE SET: set status state to track if package delivered or not delivered

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,13 +1,27 @@
-import React from "react";
+/*
+- useState expects two items in its array
+  1. name of state variable we want to keep track of
+  2. function to change the state of the variable 
+*/
+
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 // for the sake of keeping things simple for this app, we won't import the app, we will just create it here
 // we want to add state to our app component
 function App() {
+  const [status, setStatus] = useState("Not Delivered");
   return (
     <div>
-      <h1>This package is:</h1>
+      <h1>This package is: {status}</h1>
+      <button
+        onClick={() => {
+          setStatus("Delivered");
+        }}
+      >
+        Deliver
+      </button>
     </div>
   );
 }
