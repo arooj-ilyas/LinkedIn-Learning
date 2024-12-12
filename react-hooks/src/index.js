d4d6268c0fdb5c1b11a4835842a0ed2377925665
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-12 15:26:50 -0500

    useReducer used to dispatch actions

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -16,13 +16,37 @@ import React, { useReducer } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const initialState = {
+  message: "hi",
+};
+
+// this reducer function takes in the previous state and an action, and returns a new state
+// the real benefit of having access to that prev state is so that we can use it (ESPECIALLY if our new state relies on our old state)
+// this feature is especially useful in calculation logic and/or checking against previous values to make sure you are returning the new thing that's correct
+// use a switch statement to determine which state is returned
+function reducer(state, action) {
+  // take a look at whatever the action type is, and define a list of possible actions to dispatch based on that type
+  switch (action.type) {
+    case "yell":
+      return {
+        message: `HEY! I JUST SAID ${state.message}`,
+      };
+    case "whisper":
+      return {
+        message: `excuse me, I just said ${state.message}`,
+      };
+  }
+}
+
 function App() {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
+  const [state, dispatch] = useReducer(reducer, initialState); // define reducer and initialState outside of these parenthesis
 
   return (
     <>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      {checked ? "checked" : "not checked"}
+      <p>Message: {state.message}</p>
+      {/* once reducer is defined, we have message displaying with initial state but we want to try to dispatch some actions by adding some UI elements to trigger changes and dispatch actions */}
+      <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
+      <button onClick={() => dispatch({ type: "whisper" })}>whisper</button>
     </>
   );
 }
---
+++ react-hooks/src/useReducer.js
@@ -0,0 +1,10 @@
+// function App() {
+//   const [checked, toggle] = useReducer((checked) => !checked, false);
+
+//   return (
+//     <>
+//       <input type="checkbox" value={checked} onChange={toggle} />
+//       {checked ? "checked" : "not checked"}
+//     </>
+//   );
+// }
