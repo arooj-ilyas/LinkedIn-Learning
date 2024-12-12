e9b517095f97bce67b3122124f624250fbdcbb2f
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 22:51:37 -0500

    ADDED props to App.js component with name value to make it dynamically render name value

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,16 @@
+/*
+- The most common place object destucturing is used is inside of the component 
+  - we can make components dynamic using 'props' and passing in values when the component is being called 
+  - props.name can be destructured to { name }
+  - destructuring can also be used with arrays; and can be seen to be used quite often with react hooks
+*/
+
 import "./App.css";
 
-function App() {
+function App({ name }) {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello {name}!</h1>
     </div>
   );
 }
