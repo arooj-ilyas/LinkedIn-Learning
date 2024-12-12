1e906e8110894083bae8e7086fcd7de7a2f15ca5
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-12 16:15:20 -0500

    CREATED file for custom hook useInput

---
+++ react-hooks/src/useInput.js
@@ -0,0 +1,2 @@
+// if you find you are writting a lot of the same code in your component, you can extract the logic into a custom hook
+// e.g. in the previous form input elements (in useRef.js) we are just getting and resetting the values -> field takes something in, we grab it and send it somewhere and then reset it
