e21f2c495e9f0433e60cff0145f009e4248f8b4f
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-12 15:30:12 -0500

    EXTRACTED useReducer logic to its own file, now learning useRef

--- react-hooks/src/useReducer.js
+++ react-hooks/src/useReducer.js
@@ -8,3 +8,33 @@
 //     </>
 //   );
 // }
+
+// const initialState = {
+//     message: "hi",
+//   };
+// function reducer(state, action) {
+//     // take a look at whatever the action type is, and define a list of possible actions to dispatch based on that type
+//     switch (action.type) {
+//       case "yell":
+//         return {
+//           message: `HEY! I JUST SAID ${state.message}`,
+//         };
+//       case "whisper":
+//         return {
+//           message: `excuse me, I just said ${state.message}`,
+//         };
+//     }
+//   }
+
+//   function App() {
+//     const [state, dispatch] = useReducer(reducer, initialState); // define reducer and initialState outside of these parenthesis
+
+//     return (
+//       <>
+//         <p>Message: {state.message}</p>
+//         {/* once reducer is defined, we have message displaying with initial state but we want to try to dispatch some actions by adding some UI elements to trigger changes and dispatch actions */}
+//         <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
+//         <button onClick={() => dispatch({ type: "whisper" })}>whisper</button>
+//       </>
+//     );
+//   }
