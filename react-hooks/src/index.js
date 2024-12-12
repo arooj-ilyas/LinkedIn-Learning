c9b21d2dd81eeb59f85429decebdca4a8c1eb525
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-12 15:42:00 -0500

    RESET fields to empty strings once alert cleared

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -19,6 +19,7 @@ import "./index.css";
 function App() {
   // we want to track whatever the user inputs into the text field; in this case we wanna catalogue a sound the user inputs, and whatever we think the closest color to that sound may be
   // we want to be able to 'reach out' to the form element and grab whatever the user types in, and access this somewhere
+  // this allows us to check in with the values of the form elements (something you might be able to pass to db or external process) and capture user input
   const sound = useRef();
   const color = useRef();
 
@@ -28,6 +29,9 @@ function App() {
     const soundValue = sound.current.value; //.current.value is given to use by useRef fn
     const colorValue = color.current.value;
     alert(`${soundValue} sounds like ${colorValue}`);
+    // once alert is made and user has hit ok, we want fields to clear
+    sound.current.value = "";
+    color.current.value = "";
   };
 
   return (
