b39f0bd3e66e0f7d8a6fd6f5bf3691ff09e587d4
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 23:04:29 -0500

    CREATED an app component with h1 content

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -4,7 +4,13 @@ import "./index.css";
 
 // for the sake of keeping things simple for this app, we won't import the app, we will just create it here
 // we want to add state to our app component
-function App() {}
+function App() {
+  return (
+    <div>
+      <h1>This package is:</h1>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
