ba8b8c6df39eb058b8047dce69a2e16d28982406
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-11 22:55:54 -0500

    ADDED notes to explain array destructuring

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,6 +3,9 @@
   - we can make components dynamic using 'props' and passing in values when the component is being called 
   - props.name can be destructured to { name }
   - destructuring can also be used with arrays; and can be seen to be used quite often with react hooks
+  - we can use array destucturing to assign values to positions in an array
+    - const people = ["Ali", "Anna", "Alex"] -> const [first, second, third] = ["Ali", "Anna", "Alex"]
+    - If we only care for position 3 -> const [, , third] = ["Ali", "Anna", "Alex"]
 */
 
 import "./App.css";
