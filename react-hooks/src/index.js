640b825db2c9c2ea2903be27c42bae03967f17d6
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-12-12 22:03:46 -0500

    used useFetch hook to  manage all potential states (loading, error, data) when we're making http request

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -22,12 +22,25 @@ import { useFetch } from "./useFetch";
 // login is going to be the prop we pass in here, so users can see the data for their own login dynamically
 function App({ login }) {
   // our useFetch hook takes in the uri of the api we are working with
+  // want to use our predefined useFetch hook to manage all potential states (loading, error, data) when we're making http request
   const { loading, error, data } = useFetch(
     `https://api.github.com/users/${login}`
   );
   if (loading) return <h1>Loading...</h1>;
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <div>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> // good first step to return to get data visibility before trying to map; */}
+      <img src={data.avatar_url} alt={data.login} />
+      {/* display some personal info, not all users have .name prop so good to use && to check it exists first */}
+      <div>
+        <h1>{data.login}</h1>
+        {/* want to prevent app from 'breaking' if some info we want to display does not exist */}
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
